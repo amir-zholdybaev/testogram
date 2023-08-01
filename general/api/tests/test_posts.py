eadeabeca3d58011e0c2ae4b4a747a7f156033b5
amir-zholdybaev <djangodev2022@gmail.com>
2023-08-01 15:38:52 +0600

    I wrote PostTestCase for testing api publications and the test_create_post method

---
+++ general/api/tests/test_posts.py
@@ -0,0 +1,30 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from general.models import Post
+from general.factories import UserFactory, PostFactory
+
+
+class PostTestCase(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+
+        self.url = "/api/posts/"
+    
+    def test_create_post(self):
+        data = {
+            "title": "some post title",
+            "body": "some text",
+        }
+        response = self.client.post(
+            path=self.url,
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        post = Post.objects.last()
+        self.assertEqual(post.author, self.user)
+        self.assertEqual(post.title, data["title"])
+        self.assertEqual(post.body, data["body"])
+        self.assertIsNotNone(post.created_at)
